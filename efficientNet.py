76fd916a81bcf49e494f1a89a24b49b63a15387b
hongseong0314 <mi70107010@gmail.com>
2022-05-22 19:34:52 +0900

    efficientnet commit

--- efficientNet.py
+++ efficientNet.py
@@ -6,18 +6,12 @@ class mnistEfficient(torch.nn.Module):
     def __init__(self) -> None:
         super(mnistEfficient, self).__init__()
         self.backbone = EfficientNet.from_pretrained('efficientnet-b6')
-
-        self.backbone._fc = torch.nn.Linear(1000, 512)
-        self.dropout = torch.nn.Dropout(p=0.2)
-        self.activation = self.backbone._swish
-        self.classifier = torch.nn.Linear(512, 26)
+        self.classifier = torch.nn.Linear(1000, 26)
 
         torch.nn.init.xavier_normal_(self.classifier.weight)
 
     def forward(self, x):
-        x = self.backbone(x)
-        x = self.dropout(x)
-        x = self.activation(x)
+        x = F.silu(self.backbone(x))
         x = self.classifier(x)
 
         return x
